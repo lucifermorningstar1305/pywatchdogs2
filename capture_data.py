e0a14b11595a5af2919633d4edc2105b34e95c91
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2023-12-31 01:45:12 +1300

    Added: Data logging

--- capture_data.py
+++ capture_data.py
@@ -99,15 +99,15 @@ def main(file_name, starting_value):
 ##                cv2.destroyAllWindows()
 ##                break
 
-            # if len(training_data) % 100 == 0:
-            #     print(len(training_data))
+            if len(training_data) % 100 == 0:
+                print(len(training_data))
                 
-            #     if len(training_data) == 500:
-            #         np.save(file_name,training_data)
-            #         print('SAVED')
-            #         training_data = []
-            #         starting_value += 1
-            #         file_name = 'E:/pywatchdogs2/data/phase-1-st/training_data-{}.npy'.format(starting_value)
+                if len(training_data) == 500:
+                    np.save(file_name,training_data)
+                    print('SAVED')
+                    training_data = []
+                    starting_value += 1
+                    file_name = 'E:/pywatchdogs2/data/phase-1-st/training_data-{}.npy'.format(starting_value)
 
                     
         keys = key_check()
